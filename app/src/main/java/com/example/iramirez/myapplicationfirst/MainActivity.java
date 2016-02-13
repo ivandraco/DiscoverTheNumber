ce031f79c9cb1893077c6a7ebcc4313ed57e87da
unknown <iramirez@IRAMIREZ.netintellego.com>
2016-02-12 23:17:50 -0600

    Initial commit

---
+++ app/src/main/java/com/example/iramirez/myapplicationfirst/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.iramirez.myapplicationfirst;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab2 = (FloatingActionButton) findViewById(R.id.fab2);
+        fab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+           //     intent.putExtra();
+                startActivity(intent);
+
+
+            }
+        });
+
+
+    }
+
+}
